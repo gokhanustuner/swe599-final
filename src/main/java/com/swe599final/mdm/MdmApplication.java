f645dd76b5e3659fc64936e0ea80e81db90ad7f4
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-04-09 14:55:42 +0300

    Initial commit

---
+++ src/main/java/com/swe599final/mdm/MdmApplication.java
@@ -0,0 +1,13 @@
+package com.swe599final.mdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swe599final/mdm/MdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swe599final.mdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
