6859845109333b50d65e14162229e74373562688
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-05-06 21:12:53 +0300

    Remove obsolete import MdmUserDetailImplementer

---
+++ src/main/java/com/swe599final/mdm/infrastructure/model/MdmUserDetailsImplementer.java
@@ -0,0 +1,57 @@
+package com.swe599final.mdm.infrastructure.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+public class MdmUserDetailsImplementer implements MdmUserDetails {
+
+    private final User user;
+
+    public MdmUserDetailsImplementer(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    public long getId() {
+        return user.getId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public User getUser() {
+        return user;
+    }
+}
