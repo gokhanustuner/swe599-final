e76df0e77dd1fadefb2b813e6cda709f890111da
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-05-26 16:38:50 +0300

    Device Inventory UI

--- src/main/java/com/swe599final/mdm/application/controller/DeviceController.java
+++ src/main/java/com/swe599final/mdm/application/controller/DeviceController.java
@@ -2,7 +2,11 @@ package com.swe599final.mdm.application.controller;
 
 import com.google.api.services.androidmanagement.v1.model.Device;
 import com.swe599final.mdm.domain.exception.EnterpriseNotFoundByUserIdException;
+import com.swe599final.mdm.domain.service.DashboardService;
 import com.swe599final.mdm.domain.service.DeviceService;
+import com.swe599final.mdm.infrastructure.model.Enterprise;
+import com.swe599final.mdm.infrastructure.model.EnterpriseDto;
+import com.swe599final.mdm.infrastructure.model.MdmUserDetailsImplementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
@@ -16,12 +20,23 @@ import java.util.List;
 public final class DeviceController {
 
     @Autowired
-    DeviceService deviceService;
+    private DeviceService deviceService;
+
+    @Autowired
+    private DashboardService dashboardService;
 
     @GetMapping("/devices/list")
     public String listDevices(Authentication principal, Model model) throws IOException, EnterpriseNotFoundByUserIdException {
+        Enterprise applicationUsersEnterprise =
+                dashboardService.getApplicationUsersEnterprise((MdmUserDetailsImplementer) principal.getPrincipal());
+
         List<Device> devices = deviceService.listDevices(principal);
-        model.addAttribute("devices", devices);
+        model.addAttribute("devices", devices)
+            .addAttribute("enterpriseDto", new EnterpriseDto())
+            .addAttribute("principalName", principal.getName())
+            .addAttribute("applicationUserHasEnterprise", applicationUsersEnterprise != null)
+            .addAttribute("applicationUsersEnterprise", applicationUsersEnterprise)
+            .addAttribute("principalId", ((MdmUserDetailsImplementer) principal.getPrincipal()).getId());
 
         return "device_list";
     }
