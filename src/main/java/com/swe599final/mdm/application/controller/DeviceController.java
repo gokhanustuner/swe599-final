2dfb3dc3b6ac3c32bfc0cfbcf4af213182c6f8bc
Gökhan Üstüner <gokhanustuner@gmail.com>
2022-05-26 20:43:17 +0300

    Device Inventory UI improvements and delete device functionality

--- src/main/java/com/swe599final/mdm/application/controller/DeviceController.java
+++ src/main/java/com/swe599final/mdm/application/controller/DeviceController.java
@@ -4,14 +4,15 @@ import com.google.api.services.androidmanagement.v1.model.Device;
 import com.swe599final.mdm.domain.exception.EnterpriseNotFoundByUserIdException;
 import com.swe599final.mdm.domain.service.DashboardService;
 import com.swe599final.mdm.domain.service.DeviceService;
-import com.swe599final.mdm.infrastructure.model.Enterprise;
-import com.swe599final.mdm.infrastructure.model.EnterpriseDto;
-import com.swe599final.mdm.infrastructure.model.MdmUserDetailsImplementer;
+import com.swe599final.mdm.infrastructure.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.IOException;
 import java.util.List;
@@ -36,8 +37,24 @@ public final class DeviceController {
             .addAttribute("principalName", principal.getName())
             .addAttribute("applicationUserHasEnterprise", applicationUsersEnterprise != null)
             .addAttribute("applicationUsersEnterprise", applicationUsersEnterprise)
-            .addAttribute("principalId", ((MdmUserDetailsImplementer) principal.getPrincipal()).getId());
+            .addAttribute("principalId", ((MdmUserDetailsImplementer) principal.getPrincipal()).getId())
+            .addAttribute("deleteDevice", new DeleteDeviceDto());
 
         return "device_list";
+    }
+
+    @PostMapping("/devices/delete")
+    public RedirectView deleteDevice(DeleteDeviceDto deleteDeviceDto, Authentication principal, RedirectAttributes redirectAttributes) {
+        try {
+            deviceService.deleteDevice(deleteDeviceDto.getName());
+            redirectAttributes.addFlashAttribute("success", true);
+
+            return new RedirectView("/dashboard");
+        } catch (Exception e) {
+            redirectAttributes.addFlashAttribute("success", false);
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
+
+            return new RedirectView("/dashboard");
+        }
     }
 }
---
+++ src/main/java/com/swe599final/mdm/infrastructure/model/DeleteDeviceDto.java
@@ -0,0 +1,13 @@
+package com.swe599final.mdm.infrastructure.model;
+
+public class DeleteDeviceDto {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
